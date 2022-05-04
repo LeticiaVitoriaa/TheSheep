bf9139bcb9f04cc5c9df013dab8c8cdb9f782bb0
Letícia Vitória <leticiav_gomes@hotmail.com>
2022-05-04 12:58:57 -0300

    Atualizando Jogo,js

--- jogo.js
+++ jogo.js
@@ -1,51 +1,82 @@
+const largura_jogo = 800;
+const altura_jogo = 600;
+
+//constrói a sprite principal, ovelha
 class Sprite {
-            constructor(x, y, w, he, im) {
+            constructor(x, y, wid, he, imagem) {
                 this.x = x;
                 this.y = y;
-                this.w = w;
+                this.wid = wid;
                 this.he = he;
-                this.im = im;
-                //this.veloc = 50;
-                //this.pos = 240;
+                this.imagem = imagem;
             }
 
             desenhar(ctx) {
-                if (this.im) {
-                    ctx.drawImage(this.im, this.x, this.y, this.w, this.he);                    
+                if (this.imagem) {
+                    ctx.drawImage(this.imagem, this.x, this.y, this.wid, this.he);                    
                 }
                 else 
-                    ctx.strokeRect(this.x, this.y, this.w, this.he);   
+                    ctx.strokeRect(this.x, this.y, this.wid, this.he);   
             }
         }
+// constrói sprite Nave
+class Nave extends Sprite {
+    constructor() {
+        super(Math.random() * (largura_jogo - 100), altura_jogo, 100, 100, nave);
+        this.velocidadeY = -2 * Math.random() - 1;
+    }
+
+    atualizar() {
+        this.y += this.velocidadeY;
+
+        if(this.y + this.he < 0) {
+            this.y = altura_jogo;
+            this.x = Math.random() * (largura_jogo - 80);
+        }
+    }
+}
 
         let canvasEl = document.querySelector('#jogo');
         let ctx = canvasEl.getContext('2d');
+        ctx.imageSmoothingEnabled = false;
         
         let vaquinhaImagem = new Image();
         vaquinhaImagem.src = 'imgs/ovelhinha.gif';
-        let vaquinha =  new Sprite(440, 500, 80, 80, vaquinhaImagem);
+        let vaquinha =  new Sprite(440, 500, 100, 100, vaquinhaImagem);
 
-        let nave =  new Image();
-        nave.src = 'imgs/nave.png';
-        let naveImg = []
-        naveImg.push(new Sprite(350, 30, 100, 100, nave));        
-        
         vaquinhaImagem.addEventListener('load', (e) => {
             desenhajogo();
-        });
+        })
+
+        let nave = new Image();
+        nave.src = 'imgs/nave.png';
+        
+        let naveImg = [];
+        naveImg.push(new Nave());    
+        naveImg.push(new Nave()); 
+        naveImg.push(new Nave());     
+        ;
         
         nave.addEventListener('load', (e) => {
             desenhajogo();
         });
         
+        
+        //move a ovelhinha de acordo com o mouse
         canvasEl.addEventListener('mousemove', (e) => {
-            vaquinha.x = e.offsetX - (vaquinha.w/2);
+            vaquinha.x = e.offsetX - (vaquinha.wid/2);
             vaquinha.y = e.offsetY - (vaquinha.he/2);
             desenhajogo();
         });
 
-        //setInterval(desenhajogo(), 33);
+        setInterval(() => {
+            for (let nav of naveImg) {
+                nav.atualizar();
+            }
+            desenhajogo();
+        }, 33);
         
+        //desenha os personagens na tela
         function desenhajogo() {
             ctx.clearRect(0, 0, 800, 600);
             vaquinha.desenhar(ctx);
@@ -53,17 +84,4 @@ class Sprite {
             for(let nav of naveImg) {
                 nav.desenhar(ctx);
             }
-            
-            ctx.strokeStyle = 'yellowgreen';
-            //ctx.globolAlpha = 0.5;
-            ctx.fillStyle = 'rgba(170, 204, 131, 0.5)';
-            
-            ctx.beginPath();
-            ctx.moveTo(400, 126);
-            ctx.lineTo(325, 300);
-            ctx.lineTo(490, 300)
-            ctx.closePath();
-            ctx.fill();
-            ctx.stroke();
         }
-        
